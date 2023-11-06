8d4e7666f47b884a76373102e26d8c76e99adbdf
Keegan Shaw <keegan@highwoodemissions.com>
2023-11-05 22:52:32 -0700

    New features - midi routing and debounce
    Closes #3
    Closes #2
    Closes #1

--- code.py
+++ code.py
@@ -8,8 +8,9 @@ import adafruit_midi
 import busio
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
-from midi_note_map import note_mapping
-from organ_display import clear_display
+from adafruit_midi.control_change import ControlChange
+from midi_note_map import note_mapping, pot_mapping_normal, pot_mapping_opt
+from organ_display import ODisplay, clear_display, set_text_1, set_text_2, reset_text_1, reset_text_2
 # Import the SSD1306 module.
 import adafruit_ssd1306
 from analog_pin import scale_analog_in
@@ -47,9 +48,9 @@ option_pin = board.GP20
 
 # Di Pins - Internal
 control_pins = [
-    board.GP13,
-    board.GP14,
     board.GP15,
+    board.GP14,
+    board.GP13,
     board.GP19,
 ]
 
@@ -57,10 +58,12 @@ control_pins = [
 option_btn = digitalio.DigitalInOut(option_pin)
 option_btn.direction = digitalio.Direction.INPUT
 option_btn.pull = digitalio.Pull.UP
+
 organ_btns = [digitalio.DigitalInOut(bp) for bp in button_pins]
 for btn in organ_btns:
     btn.direction = digitalio.Direction.INPUT
     btn.pull = digitalio.Pull.UP
+
 ctrl_btns = [digitalio.DigitalInOut(bp) for bp in control_pins]
 for btn in ctrl_btns:
     btn.direction = digitalio.Direction.INPUT
@@ -70,137 +73,148 @@ for btn in ctrl_btns:
 # Key and trigger states - Organ Pedals
 pressed_keys = [False for _ in button_pins]
 triggered_keys = [False for _ in button_pins]
-tr_key_cnt = [0 for _ in button_pins]
+tr_key_dtime = [0 for _ in button_pins]
 
 # Key and trigger states - Midi Buttons
-pressed_keys_midi = [False for _ in control_pins]
-triggered_keys_midi = [False for _ in control_pins]
-
+pressed_ctl_btns = [False for _ in control_pins]
+trig_ctl_btns = [False for _ in control_pins]
 
 # Ai Pins - Internal
-orgin_vol_pin = analogio.AnalogIn(board.GP28)
-orgin_filter_pin = analogio.AnalogIn(board.GP26)
-pot_2 = analogio.AnalogIn(board.GP27)
+pots = [analogio.AnalogIn(board.GP28), analogio.AnalogIn(board.GP27), analogio.AnalogIn(board.GP26)]
+pot_vals = [scale_analog_in(pot) for pot in pots]
+showing_pots_cnt = 0
 
+# ------- Initialize Display --------
+o_display = ODisplay(display, "Play", '%s' % note_mapping[0])
+
+
+# init internal vars
 transpose = 0
-option_on = False
 option_mode = False
-
-# Clear the display.  Always call show after changing pixels to make the display
-# update visible!
-clear_display(display)
-display.text('Play', 0, 20,1,size=1)
-display.text('%s' % note_mapping[0], 62, 15,1,size=2)
-display.show()
+option_on_flag = False # Flag for denoting option button has been engaged
+option_on = False # Flag for denoting option button has been engaged
+debounce_time = 0.001 
 
 print("MacroPad MIDI Board")
-
-vol =scale_analog_in(orgin_vol_pin)
-filter = scale_analog_in(orgin_filter_pin)
-showing_vol = False
-showing_vol_cnt = 0
-showing_filter = False
-showing_filter_cnt = 0
-
-last_filter_val = 0
 # Run Main Routine
 while True:
-    new_filt = scale_analog_in(orgin_filter_pin)
-    if abs(new_filt-filter) > 4 :
-        filter  = new_filt
-        print("Frequency %s " % new_filt)
-    
-    # If volume has
-    if showing_vol:
-        showing_vol_cnt += 1
-    # Only show Volume for 80 cycles
-    if showing_filter:
-        showing_filter_cnt += 1
-    # Only show Filter for 80 cycles
-    if showing_vol_cnt > 80:
-        showing_vol_cnt = 0
-        if option_mode:
-            clear_display(display)
-            display.text('Options', 0, 20,1,size=1)
-            display.text('%s' % note_mapping[0], 62, 15,1,size=2)
-            display.show()
-        else:
-            clear_display(display)
-            display.show()
-            display.text('Play', 0, 20,1,size=1)
-            display.text('%s' % note_mapping[transpose], 62, 15,1,size=2)
-            display.show()
-        showing_vol = False
-    new_vol =scale_analog_in(orgin_vol_pin)
-    if abs(vol-new_vol) > 4:
-        vol = new_vol
-        clear_display(display)
-        display.text('Volume', 0, 20,1,size=1)
-        display.text(str(vol), 62, 15,1,size=2)
-        display.show()
-        showing_vol = True
-    if not option_btn.value and not option_on:
-        option_on = True
-        option_mode = not option_mode
-        if option_mode:
-            clear_display(display)
-            display.text('Options', 0, 20,1,size=1)
-            display.show()
-        else:
-            clear_display(display)
-            display.text('Play', 0, 20,1,size=1)
-            display.text('%s' % note_mapping[transpose], 62, 15,1,size=2)
-            display.show()
-        print("Option mode - %s" % option_mode)
-    if option_btn.value and option_on:
-        option_on = False
-        print("Option Released")
+    # Initialize Key Vals
+    current_time = time.monotonic()
+    for ix, btn in enumerate(organ_btns):
+        button_act = not btn.value
+        # Button is activated not conditioned
+        if button_act and not pressed_keys[ix]:
+            # If hasnt started
+            if tr_key_dtime[ix] == 0:
+                tr_key_dtime[ix] = current_time
+            # Mark and clear timer if condition met 
+            elif current_time - tr_key_dtime[ix] >= debounce_time:
+                pressed_keys[ix] = True
+                tr_key_dtime[ix] = 0 
+        elif not button_act:
+            # Could add a debouce timer on fall too
+            pressed_keys[ix] = False
 
     for ix, btn in enumerate(organ_btns):
-        pressed_keys[ix] = btn.value
+        if tr_key_dtime[ix] > 0 and current_time - tr_key_dtime[ix] >= debounce_time:
+                print(str(current_time - tr_key_dtime[ix]))
+                pressed_keys[ix] = not btn.value
+                tr_key_dtime[ix] = 0
+
 
     for ix, btn in enumerate(ctrl_btns):
-        pressed_keys_midi[ix] = btn.value
+        pressed_ctl_btns[ix] = not btn.value
+
+    # ---- Handle Options ----
+    # On Click - option_btn.value is fail safe
+    if not option_btn.value and not option_on_flag:
+        # After Value has been on for more then a scan, d
+        option_mode = not option_mode
+        option_on_flag = True
+        if option_mode:
+            # clear all keys
+            for idx, _ in enumerate(button_pins):
+                if pressed_keys[idx]:
+                    pressed_keys[idx] = False
+                if triggered_keys[idx]:
+                    triggered_keys[idx] = False
+                if pressed_keys[idx] or triggered_keys[idx]:
+                    midi.send([NoteOff(48 + ix + transpose, 0)])
+            o_display.set_text_1("Options")
+            o_display.set_text_2('-')
+        else:
+            # clear all keys
+            for idx, _ in enumerate(control_pins):
+                if pressed_ctl_btns[idx]:
+                    pressed_ctl_btns[idx] = False
+                if trig_ctl_btns[idx]:
+                    trig_ctl_btns[idx] = False
+                if pressed_ctl_btns[idx] or trig_ctl_btns[idx]:
+                    midi_2.send([NoteOff(36 + ix, 0)])
+            o_display.set_text_1("Play")
+            o_display.set_text_2('%s' % note_mapping[transpose])
+    # On Release - option_btn.value is fail safe
+    if option_btn.value and option_on_flag:
+        option_on_flag = False
+
+
+    # Potentiometer Controls
+    new_pot_vals = [scale_analog_in(pot) for pot in pots]
+    for ix, (nv, v) in enumerate(zip(new_pot_vals, pot_vals)):
+        if abs(nv-v) > 4:
+            if option_mode:
+                o_display.set_text_1(pot_mapping_opt[ix][0])
+                midi_2.send([ControlChange(pot_mapping_opt[ix][1], nv)])
+            else:
+                o_display.set_text_1(pot_mapping_normal[ix][0])
+                midi_2.send([ControlChange(pot_mapping_normal[ix][1], nv)])
+            o_display.set_text_2(str(nv))
+            showing_pots_cnt=1
+            pot_vals[ix] = nv
+    # Clear Pot controls if they are idle for more than 500 cycles
+    if showing_pots_cnt > 0:
+        if showing_pots_cnt < 500:
+            showing_pots_cnt = showing_pots_cnt + 1
+        else:
+            if option_mode:
+                o_display.set_text_1("Options")
+                o_display.set_text_2('-')
+            else:
+                o_display.set_text_1("Play")
+                o_display.set_text_2('%s' % note_mapping[transpose])
+            showing_pots_cnt = 0
 
     # Organ Buttonns
     for ix, (pk, tk) in enumerate(zip(pressed_keys, triggered_keys)):
-        # IF key is pressed (reversed pol) but not yet Triggered
-        if not pk and not tk:
-            volume = round(vol)
+        # IF key is pressed but not yet Triggered
+        if pk and not tk:
             triggered_keys[ix] = True
             if not option_mode:
-                midi.send([NoteOn(48 + ix + transpose, volume )])
-
+                midi.send([NoteOn(48 + ix + transpose, 127 )])
             else:
-                clear_display(display)
-                display.text('Options', 0, 20,1,size=1)
-                display.text(note_mapping[ix], 62, 15,1,size=2)
-                display.show()
-                print("option-organ %s pressed" % ix)
-                midi.send([NoteOff(48 + ix + transpose, 0)])
+                o_display.set_text_1("Options")
+                o_display.set_text_2('%s' % note_mapping[ix])
                 transpose = ix
 
-        elif pk and tk:
-        # IF key is not being pressed (reversed pol)and was triggered
+        elif not pk and tk:
+        # IF key is not being pressed and was triggered
             triggered_keys[ix] = False
             if not option_mode:
                 midi.send([NoteOff(48 + ix + transpose, 0)])
 
-    # Midi Buttoons
-    for ix, (pk, tk) in enumerate(zip(pressed_keys_midi, triggered_keys_midi)):
-        if not pk and not tk:
-            triggered_keys_midi[ix] = True
+    # Control Buttons
+    for ix, (pk, tk) in enumerate(zip(pressed_ctl_btns, trig_ctl_btns)):
+        if pk and not tk:
+            trig_ctl_btns[ix] = True
             if not option_mode:
                 print("midi %s started" % note_mapping[ix])
                 midi_2.send([NoteOn(36 + ix, 127)])
             else:
                 print("option-midi %s pressed" % note_mapping[ix])
 
-        elif pk and tk:
-            triggered_keys_midi[ix] = False
+        elif not pk and tk:
+            trig_ctl_btns[ix] = False
             if not option_mode:
                 print("midi %s stopped" % note_mapping[ix])
                 midi_2.send([NoteOff(36 + ix, 0)])
 
-
-    time.sleep(0.001)
--- lib/organ_display/__init__.py
+++ lib/organ_display/__init__.py
@@ -1,3 +1,84 @@
+class ODisplay:
+    def __init__(self, display_obj, text1, text2):
+        self.display = display_obj
+        self.text1 = text1
+        self.text1_pos = [0, 20]
+        self.text1_size = 1
+        self.text2 = text2
+        self.text2_pos = [62, 15]
+        self.text2_size = 2
+        
+        self.clear()
+        self.set_text_1(text1)
+        self.set_text_2(text2)
+
+    def clear(self):
+        self.display.fill(0)
+        self.display.show()
+    def set_text_1(self, text):
+        # Clear Display
+        self.display.text(
+            self.text1, 
+            self.text1_pos[0], 
+            self.text1_pos[1],
+            0,
+            size=self.text1_size)
+        self.display.show() 
+        
+        #Set Variable
+        self.text1 = text
+
+        # Set Display
+        self.display.text(
+            self.text1, 
+            self.text1_pos[0], 
+            self.text1_pos[1],
+            1,
+            size=self.text1_size)
+        self.display.show()    
+
+    def set_text_2(self, text):
+        # Clear Display
+        self.display.text(
+            self.text2, 
+            self.text2_pos[0], 
+            self.text2_pos[1],
+            0,
+            size=self.text2_size)
+        self.display.show() 
+        
+        #Set Variable
+        self.text2 = text
+
+        # Set Display
+        self.display.text(
+            self.text2, 
+            self.text2_pos[0], 
+            self.text2_pos[1],
+            1,
+            size=self.text2_size)
+        self.display.show()    
+
+
+
+
+
 def clear_display(display_obj):
+    display_obj.fill(0)
+    display_obj.show()
+
+def set_text_1(text, display, size=1):
+    display.text(text, 0, 20,1,size=size)
+    display.show()
+
+def set_text_2(text, display, size=2):
+    display.text(text, 62, 15,1,size=size)
+    display.show()
+
+def reset_text_1(prev_text, display_obj):
+    display_obj.fill(0)
+    display_obj.show()
+
+def reset_text_2(prev_text, display_obj):
     display_obj.fill(0)
     display_obj.show()
